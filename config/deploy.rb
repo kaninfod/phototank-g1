9a35b751bfde4d42b9223e59ea3c91ffaa6daea8
kaninfod <martinhinge@gmail.com>
2016-02-01 20:25:21 +0100

    l

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # Change these
-server '192.168.2.103', port: 22, roles: [:web, :app, :db], primary: true, :worker
+server '192.168.2.103', port: 22, roles: [:web, :app, :db, :worker], primary: true, 
 
 set :repo_url,        'git@github.com:kaninfod/phototank-rails.git'
 set :application,     'phototank'
