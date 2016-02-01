5b70a6ca91117a79b28c206f26fc9f08a06b431d
kaninfod <martinhinge@gmail.com>
2016-02-01 19:19:23 +0100

    l

--- config/deploy.rb
+++ config/deploy.rb
@@ -24,8 +24,8 @@ set :puma_worker_timeout, nil
 set :puma_init_active_record, true  # Change to false when not using ActiveRecord
 
 ## Defaults:
-# set :scm,           :git
-# set :branch,        :master
+set :scm,           :git
+set :branch,        :master
 # set :format,        :pretty
 # set :log_level,     :debug
 # set :keep_releases, 5
