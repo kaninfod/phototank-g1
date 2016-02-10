c9c72fa0cd872d3a2c1c38e8fc992b7450c9f744
kaninfod <martinhinge@gmail.com>
2016-02-10 10:02:50 +0100

    Minor - config

--- app/controllers/administration_controller.rb
+++ app/controllers/administration_controller.rb
@@ -1,22 +1,21 @@
 class AdministrationController < ApplicationController
-  
-  def generate_timebased_albums
-    
-    distinct_years = Photo.select("strftime('%Y', date_taken) as year").distinct.each do |rec|
-      album = Album.new
-      album.name = 'Photos taken in ' + rec.year.to_s
-      album.start = Date.new(rec.year.to_i, 1, 1)
-      album.end = Date.new(rec.year.to_i, 12, 31)
-      album.save
 
+  def generate_timebased_albums
+    byebug
+    distinct_years = Photo.pluck(:date_taken).map{|x| x.year}.uniq.each do |rec|
+      album = Album.new
+      album.name = 'Photos taken in ' + rec.to_s
+      album.start = Date.new(rec.to_i, 1, 1)
+      album.end = Date.new(rec.to_i, 12, 31)
+      album.save
     end
 
-    distinct_months = Photo.select("strftime('%Y-%m-01', date_taken) as year").distinct.each do |rec|
+    distinct_months = Photo.pluck(:date_taken).map{|x| Date.new(x.year, x.month, 1)}.uniq.each do |rec|
       album = Album.new
-      start_date = Date.parse rec.year
-      album.name = 'Photos taken in ' + start_date.strftime("%b %y")
-      album.start = start_date
-      album.end = (start_date >> 1) -1
+      #tart_date = Date.parse rec
+      album.name = 'Photos taken in ' + rec.strftime("%b %y")
+      album.start = rec
+      album.end = (rec >> 1) -1
       album.save
 
     end
@@ -32,16 +31,16 @@ class AdministrationController < ApplicationController
       @info[queue] = Resque.size(queue)
     end
     workers=[]
-    Resque.workers.each do |worker| 
+    Resque.workers.each do |worker|
       workers.push({worker => worker.working?})
-      
+
     end
     @info["workers"] = workers
     respond_to do |format|
       format.html { }
       format.json { render json: @info }
     end
-    
-  
+
+
   end
 end
