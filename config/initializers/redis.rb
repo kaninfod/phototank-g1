1c487ab48e3e621efb9b192edad18499a47a2209
kaninfod <martinhinge@gmail.com>
2016-05-28 08:36:33 +0200

    r

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,2 +1,2 @@
 #$redis = Redis.new(:host => ENV["REDIS_HOST"], :port => ENV["REDIS_PORT"])
-$redis = Redis.new(:host => "192.168.2.103", :port => 6379)
+#$redis = Redis.new(:host => "192.168.2.103", :port => 6379)
