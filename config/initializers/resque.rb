87cc4b01a0a2f3e6552701c9ae13689902ce0446
kaninfod <martinhinge@gmail.com>
2016-05-29 16:36:56 +0200

    r

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,8 +1,18 @@
-rails_root = ENV['RAILS_ROOT'] || File.dirname(__FILE__) + '/../..'
-rails_env = ENV['RAILS_ENV'] || 'development'
+#require 'resque/failure/multiple'
+#require 'resque/failure/redis'
+#Resque::Failure::Multiple.classes = [Resque::Failure::Redis]
+#Resque::Failure.backend = Resque::Failure::Multiple
+#Dir[File.join(Rails.root, 'app', 'jobs', '*.rb')].each { |file| require file }
+config = YAML.load(File.open("#{Rails.root}/config/resque.yml"))[Rails.env]
+Resque.redis = Redis.new(host: config['host'], port: config['port'], db: config['db'])
 
-resque_config = YAML.load_file(rails_root + '/config/resque.yml')
-Resque.redis = resque_config[rails_env]
+
+
+#rails_root = ENV['RAILS_ROOT'] || File.dirname(__FILE__) + '/../..'
+#rails_env = ENV['RAILS_ENV'] || 'development'
+
+#resque_config = YAML.load_file(rails_root + '/config/resque.yml')
+#Resque.redis = resque_config[rails_env]
 Rails.logger.warn Resque.redis
 
 
