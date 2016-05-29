b7078b0c913352e02b6f7147eb9ecf51d40cb02c
kaninfod <martinhinge@gmail.com>
2016-05-29 11:24:46 +0200

    p

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -3,6 +3,9 @@ rails_env = ENV['RAILS_ENV'] || 'development'
 
 resque_config = YAML.load_file(rails_root + '/config/resque.yml')
 Resque.redis = resque_config[rails_env]
+Rails.logger.warn Resque.redis
+
+
 
 Resque.logger = MonoLogger.new(File.open("#{Rails.root}/log/resque.log", "w+"))
 Resque.logger.formatter = Resque::QuietFormatter.new
