f66d59e16f51c2d7323c46da5bcaa76dbacd28e9
kaninfod <martinhinge@gmail.com>
2016-07-29 21:01:50 +0200

    log

--- app/jobs/spawn_import_master.rb
+++ app/jobs/spawn_import_master.rb
@@ -3,14 +3,14 @@ class SpawnImportMaster < ResqueJob
   @queue = :import
 
   def self.perform(path, photo_id= nil, import_mode=true)
-    Resque.logger.info path
+    logger.info path
     begin
       Dir.glob("#{path}/**/*.jpg").each do |import_file_path|
-        Resque.logger.info import_file_path
+        logger.info import_file_path
         if File.file? import_file_path
           Resque.enqueue(PhotoImportMaster, import_file_path, photo_id, import_mode)
         else
-          raise "no file to import: #{import_file_path}" 
+          raise "no file to import: #{import_file_path}"
         end
       end
 
