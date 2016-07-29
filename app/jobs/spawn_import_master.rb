871bfc93e173afde3582a2c7947d592a34917f1e
kaninfod <martinhinge@gmail.com>
2016-07-29 21:19:33 +0200

    log

--- app/jobs/spawn_import_master.rb
+++ app/jobs/spawn_import_master.rb
@@ -1,12 +1,14 @@
 class SpawnImportMaster < ResqueJob
   include Resque::Plugins::UniqueJob
   @queue = :import
+  @logger = Logger.new File.join(Rails.root, 'log', 'resque.log')
+  @logger.debug "foo bar"
 
   def self.perform(path, photo_id= nil, import_mode=true)
-    logger.info path
+    @logger.info path
     begin
       Dir.glob("#{path}/**/*.jpg").each do |import_file_path|
-        logger.info import_file_path
+        @logger.info import_file_path
         if File.file? import_file_path
           Resque.enqueue(PhotoImportMaster, import_file_path, photo_id, import_mode)
         else
