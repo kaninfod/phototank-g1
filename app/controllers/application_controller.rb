3f96b6a3a9aaa1c08173fd17f7648393ec6589cc
kaninfod <martinhinge@gmail.com>
2016-08-02 21:27:02 +0200

    pp

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,9 @@ class ApplicationController < ActionController::Base
 
 
   skip_before_action :verify_authenticity_token, if: :json_request?
-
+  before_action do
+    Rack::MiniProfiler.authorize_request
+  end
 
   private
 
