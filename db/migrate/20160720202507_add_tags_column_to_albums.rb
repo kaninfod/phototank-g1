b0ef316a33e98f314198783d4018bfb4ff56f23b
kaninfod <martinhinge@gmail.com>
2016-08-06 23:28:20 +0200

    s

--- db/migrate/20160720202507_add_tags_column_to_albums.rb
+++ db/migrate/20160720202507_add_tags_column_to_albums.rb
@@ -1,6 +1,6 @@
 class AddTagsColumnToAlbums < ActiveRecord::Migration
   def change
-    add_column :albums, :tags, :string, array: true, default: []
+    add_column :albums, :tags, :string,  default: []
     add_column :albums, :like, :boolean
   end
 end
